0c1142767fa9370703b3ff61c71c676fc84077ff
Nikhin <nikhinrajkk@gmail.com>
2023-07-31 12:00:49 +0530

    Minor fix

---
+++ src/pages/faq/[id].tsx
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        FAQ. Post: {router.query.id} Hello World!
+      </div>
+    </main>
+  );
+}
